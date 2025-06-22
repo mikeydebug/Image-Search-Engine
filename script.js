b29d0925660f8b1c7a0e405d0074fb206c0a7c21
mikeydebug <cs24b1031@iiitr.ac.in>
2025-06-22 19:03:52 +0530

    completed project

--- script.js
+++ script.js
@@ -8,7 +8,38 @@ let keyword="";
 let page =1;
 
 async function searchimage() {
-    keyword=searchbox.ariaValueMax;
-    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}`;
+    keyword = searchbox.value;
+    const url=`https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accesskey}&per_page=12`;
     
+    const responce = await fetch(url);
+    const data = await responce.json();
+
+    if(page === 1){
+        searchresult.innerHTML ="";
+    }
+    const results= data.results;
+
+    results.map((result)=>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imagelink = document.createElement("a");
+        imagelink.href=result.links.html;
+        imagelink.target = "_blank";
+        imagelink.appendChild(image);
+        searchresult.appendChild(imagelink);
+    })
+
+    showmorebtn.style.display="block";
+
 }
+
+searchform.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    page =1;
+    searchresult.innerHTML = "";
+    searchimage();
+})
+showmorebtn.addEventListener("click",()=>{
+    page++;
+    searchimage();
+})
